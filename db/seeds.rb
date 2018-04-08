5bdc255a520af326c7e5a803414fb5403ed566f2
Cristi Zanfir <cristian.zanfir@unifiedpost.com>
2018-04-08 17:26:40 +0300

    Added index view for posts. Updated tests and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
+User.create(email: 'test@test.com', password: 'testtest', first_name: 'First', last_name: 'Last')
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user: User.first)
 end
 
 puts "100 Posts have been created"
