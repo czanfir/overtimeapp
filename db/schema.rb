afefd8ea8f51ea332fd488db156311aa20d57d89
Cristi Zanfir <cristian.zanfir@unifiedpost.com>
2018-04-13 19:11:58 +0300

    Changed enum for posts to be able to search for status in admin dashboard. Added radio buttons and spec for edit/new form

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20180413135344) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.integer "status", default: 0
+    t.text "status", default: "submitted"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
