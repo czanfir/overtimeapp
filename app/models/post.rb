5b4ddf72d2172d1bf1969e2bbada662cf6aa170e
Cristi Zanfir <cristian.zanfir@unifiedpost.com>
2018-04-18 17:57:21 +0300

    Refactored post indexing with a custom scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   enum status: { submitted: 'submitted', approved: 'approved', rejected: 'rejected' }
   validates_presence_of :date, :rationale
   belongs_to :user
+
+  scope :posts_by, ->(user) { where(user_id: user.id) }
 end
